ff64bb9354b651e977b0621f91fbe85ac7bb23b9
patkis <p.kiss8888@gmail.com>
2023-01-13 17:47:26 +0100

    some  commit 2

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // Enable body parser
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// // Set static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/openai', require('./routes/openaiRoutes'));
+app.use('/openai', require('./routes/openaiRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
